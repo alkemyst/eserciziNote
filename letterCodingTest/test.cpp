3b154813cd014c1cc1ad9b3995d669f98c7f1b5f
Stefano Mersi <stefano.mersi@gmail.com>
2020-11-10 13:27:15 +0100

    Cleanup of d fixind length of coding

--- letterCodingTest/test.cpp
+++ letterCodingTest/test.cpp
@@ -6,6 +6,7 @@
 
 const std::string vowels = "012";
 const std::string goodConsonants = "OI";
+const std::string separators = "-+.";
 
 std::string encode(const int originalInput, const std::string &myCode) {
   int currentInput = originalInput;
@@ -20,30 +21,35 @@ std::string encode(const int originalInput, const std::string &myCode) {
   digitValue = 1;
   for (int i = myCode.length() - 1; i >= 0; --i) {
     aLetter = myCode[i];
-    for (const auto digits : myDigits) {
-      if (digits->find(aLetter) != std::string::npos) {
+    if (separators.find(aLetter) != std::string::npos) {
+      result = "-" + result;
+    } else
+      for (const auto digits : myDigits) {
+        if (digits->find(aLetter) != std::string::npos) {
 
-        // There are nDigits digits in this position
-        nDigits = digits->length();
+          // There are nDigits digits in this position
+          nDigits = digits->length();
 
-        // This digit is ...
-        thisDigit = currentInput % nDigits;
+          // This digit is ...
+          thisDigit = currentInput % nDigits;
 
-        // Let's update the final result
-        result = digits->at(thisDigit) + result;
+          // Let's update the final result
+          result = digits->at(thisDigit) + result;
 
-        // I remove the digit and divide the current input value
-        currentInput -= thisDigit;
-        currentInput /= nDigits;
+          // I remove the digit and divide the current input value
+          currentInput -= thisDigit;
+          currentInput /= nDigits;
 
-        // And write the result we are actually encoding here
-        // and also update digitValue for the next digit, if any
-        compute += thisDigit * digitValue;
-        digitValue *= nDigits;
+          // And write the result we are actually encoding here
+          // and also update digitValue for the next digit, if any
+          compute += thisDigit * digitValue;
+          digitValue *= nDigits;
+        }
       }
+#ifdef trunkation
       if (currentInput == 0)
         break;
-    }
+#endif
   }
 
   // Final check to make sure the coding went right
